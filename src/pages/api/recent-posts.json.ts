d0f565e577c75d312c958b7e56602c1e2087961e
Henry <tongsik98@gmail.com>
2026-01-31 15:13:39 -0800

    add api to send recent post to portfolio

---
+++ src/pages/api/recent-posts.json.ts
@@ -0,0 +1,24 @@
+import { getCollection } from 'astro:content';
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async () => {
+  const allPosts = await getCollection('blog');
+  const recentPosts = allPosts
+    .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+    .slice(0, 4)
+    .map(post => ({
+      id: post.id,
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      categories: post.data.categories,
+      url: `/henry-blog/posts/${post.id}/`
+    }));
+
+  return new Response(JSON.stringify(recentPosts), {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
+};
